c68ae44dc2951e9c7b57bc79af8444ec6336ef42
Aragats ODROID <vaclav.stepan@odz.ujf.cas.cz>
2018-09-19 20:31:24 +0000

    Enabled debug logging for the data uploader

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo Starting AIRDOS data gatherers and upload script
 echo -n 'Date: ' 
 date
 
-${D}/data-uploader/dataUpload.py ${D}/station/aragats.json >/dev/null 2>${L}/uploader-error.log &
+${D}/data-uploader/dataUpload.py ${D}/station/aragats.json >/data/log/uploader.log 2>${L}/uploader-error.log &
 
 ${D}/station/airdos1.py >>${L}/airdos1.log 2>&1 &
 #${D}/station/airdos2.py >>${L}/airdos2.log 2>&1 &
